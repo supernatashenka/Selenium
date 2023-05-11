f331e5a85994d1800c22641b9a3acb434d0f349d
Netology QA <obgynkostina@gmail.com>
2023-05-11 20:28:21 +0300

    tests are ready

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -40,14 +40,66 @@ class SeleniumTest {
     }
 
     @Test
-    void shouldSendForm() {
+    void shouldSendFormWithValidData() {
 
-        driver.findElement( By.cssSelector("[data-test-id=name] input")).sendKeys("Петров Николай");
-        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79261234567");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79095555555");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button__text")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSendFormWithInvalidName() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ivanova Ivanna");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79295555555");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotSendFormWithEmptyName () {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79295555555");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='name'].input_invalid .input__sub")).getText();
+    }
+
+    @Test
+    public void shouldNotSendFormWithInvalidPhoneNumber() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("89295555555");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+    }
+
+    @Test
+    public void shouldNotSendFormWithEmptyPhoneNumber () {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Поле обязательно для заполнения";
+        String actual = driver.findElement(By.cssSelector("[data-test-id='phone'].input_invalid .input__sub")).getText();
+
+    }
+    @Test
+    public void EmptyCheckBox () {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79295555555");
+        //driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
+        String actual = driver.findElement(By.className("checkbox__text")).getText();
+
     }
 }
